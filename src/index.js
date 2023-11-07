6bda9ab576878f0d3092a4cf8c5d1088a8ffcce0
HuynhQuyn <huynhquyn0811@example.com>
2023-11-07 16:20:48 +0700

    final project

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+//product provider
+import ProductProvider from './contexts/ProductContext';
+//sidebar provider
+import SidebarProvider from './contexts/SidebarContext';
+import  CartContext  from './contexts/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <SidebarProvider>
+    <CartContext>
+      <ProductProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ProductProvider>
+    </CartContext>
+  </SidebarProvider>
 );
